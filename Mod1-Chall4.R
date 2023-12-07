a967d9290f154a71d5ea744230520c6592d2e217
juliannad0929 <julid24@bu.edu>
2023-12-07 11:00:19 -0500

    moving the modules to this folder

---
+++ Mod1-Chall4.R
@@ -0,0 +1,9 @@
+sayhi <- function(x) {
+  hi <- paste("greetings, ", x, "!", sep = "")
+  # the paste command allows string concatenation
+  return(hi)
+}
+name1 <- "septimus"
+name2 <- "thomasina"
+sayhi(name1)  
+sayhi(name2)
